d8ecebcbe126c97e6c849c9dd0b42b46f8bfa3d3
angel <angel.munoz.sagaseta@gmail.com>
2016-03-28 10:48:34 +0200

    Scripts: added new bash script for tests

---
+++ scripts/bash/testClean.sh
@@ -0,0 +1,8 @@
+#	Ejecuta el script de limpieza de audio varias veces seguidas para obtener estadísticas
+max = 20
+
+for ((i = 0; i <= $max; ++i ))
+do
+	echo "Test #$i"
+	python scripts/python/clean.py test/dirty/Raphael\ -\ Yo\ soy\ aquel.wav 70 1 20 1
+done
